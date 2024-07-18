3231a169036d5c7c0a203cd718e9d7fc8e6a0b40
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-07-18 16:30:50 +0530

    trying resolving error

--- SummarizeAI/settings.py
+++ SummarizeAI/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env")
+# load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
